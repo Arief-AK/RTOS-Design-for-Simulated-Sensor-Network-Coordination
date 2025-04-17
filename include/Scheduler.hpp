49365384a5012720dbe5e77058f528bb4ea59d95
arief-ak <mariefak22@gmail.com>
2025-04-17 14:57:02 +0200

    modified Schduler interface to select tasks

---
+++ include/Scheduler.hpp
@@ -0,0 +1,18 @@
+#ifndef SCHEDULER_HPP
+#define SCHEDULER_HPP
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include <TaskControlBlock.hpp>
+
+class Scheduler{
+public:
+    virtual ~Scheduler() = default;
+
+    // Method to select a task to the scheduler
+    virtual TaskControlBlock* selectTask(std::vector<std::shared_ptr<TaskControlBlock>> &ready_queue, int current_time) = 0;
+};
+
+#endif // SCHEDULER_HPP
