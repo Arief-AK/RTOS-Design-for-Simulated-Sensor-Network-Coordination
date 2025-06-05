6363309d0c7de8072e1915b73a0937c2963091ae
Arief-AK <mariefak22@gmail.com>
2025-06-05 12:21:42 +0200

    added priority member variable to TaskControlBlock

--- src/version-2.0/src/TaskControlBlock.cpp
+++ src/version-2.0/src/TaskControlBlock.cpp
@@ -6,9 +6,9 @@ TaskControlBlock::TaskControlBlock(
 ) : task_id(id), arrival_time(arrival), computation_time(computation),
     abs_deadline(deadline), rel_deadline(deadline - arrival), 
     start_time(0), finish_time(0), response_time(0), value(value),
-    tardiness(0), remaining_time(computation_time), lateness(0),
-    laxity(0), criticality(critical), status(TaskStatus::READY),
-    behaviour(nullptr) {}
+    tardiness(0), remaining_time(computation_time), priority(0),
+    lateness(0), laxity(0), criticality(critical),
+    status(TaskStatus::READY), behaviour(nullptr) {}
 
 TaskControlBlock::~TaskControlBlock() {
     // Destructor implementation (if needed)
@@ -76,11 +76,14 @@ uint8_t TaskControlBlock::getResponseTime() const { return response_time; }
 uint8_t TaskControlBlock::getValue() const { return value; }
 uint8_t TaskControlBlock::getTardiness() const { return tardiness; }
 uint8_t TaskControlBlock::getRemainingTime() const { return remaining_time; }
+uint8_t TaskControlBlock::getPriority() const { return priority; }
+
 int8_t TaskControlBlock::getLateness() const { return lateness; }
 int8_t TaskControlBlock::getLaxity() const { return laxity; }
 TaskStatus TaskControlBlock::getStatus() const { return status; }
 TaskCriticality TaskControlBlock::getCriticality() const { return criticality; }
 
+void TaskControlBlock::setPriority(uint8_t new_priority) { priority = new_priority; }
 void TaskControlBlock::setStatus(TaskStatus new_status) { status = new_status; }
 void TaskControlBlock::setStartTime(uint8_t time) { start_time = time; }
 void TaskControlBlock::setResponseTime(uint8_t time) { response_time = time; }
