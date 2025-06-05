622b5dc2f13a1f4fc91de3b8288d669184e05c51
Arief-AK <mariefak22@gmail.com>
2025-06-05 12:20:34 +0200

    added controller class

---
+++ src/version-2.0/include/Controller.hpp
@@ -0,0 +1,29 @@
+#ifndef V2_CONTROLLER_HPP
+#define V2_CONTROLLER_HPP
+
+#include <memory>
+#include <Kernel.hpp>
+#include <CABuffer.hpp>
+#include <CABuffer.tpp>
+#include <ProducerTask.hpp>
+#include <ConsumerTask.hpp>
+
+#include <RMScheduler.hpp>
+#include <PriorityScheduler.hpp>
+
+// Shared buffer for sensor data
+extern CABuffer<int, 20> sensor_data_buffer;
+
+class Controller{
+public:
+    Controller(/* args */);
+    ~Controller() = default;
+
+    void RunRMScheduler();
+    void RunPriorityScheduler();
+
+private:
+    ConsoleLogger m_logger;
+};
+
+#endif // V2_CONTROLLER_HPP
