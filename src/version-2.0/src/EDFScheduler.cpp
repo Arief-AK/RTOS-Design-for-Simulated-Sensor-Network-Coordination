742ab77c64c1af256bb99377619097b1a8d9cfb1
Arief-AK <mariefak22@gmail.com>
2025-06-12 11:11:48 +0200

    added EDFScheduler

---
+++ src/version-2.0/src/EDFScheduler.cpp
@@ -0,0 +1,15 @@
+#include <EDFScheduler.hpp>
+
+TaskControlBlock* EDF_SCHEDULER_HPP::select_next_task(std::vector<TaskControlBlock*>& task_list, uint8_t current_time) {
+    TaskControlBlock* selected_task = nullptr;
+
+    for (auto &task : task_list){
+        if(task->getStatus() == TaskStatus::READY && task->getArrivalTime() <= current_time){
+            if(!selected_task || task->getAbsDeadline() < selected_task->getAbsDeadline()){
+                selected_task = task;
+            }
+        }
+    }
+    
+    return selected_task;
+}
